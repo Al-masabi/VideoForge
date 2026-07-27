364eda33da20cd09b4131e6a574f7ff50ec4c38b
Al-masabi <136709031+Al-masabi@users.noreply.github.com>
2026-07-27 19:55:02 +0300

    Update HomeScreen.kt

--- app/src/main/kotlin/com/videoforge/android/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/com/videoforge/android/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.videoforge.android.ui.home
 
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
